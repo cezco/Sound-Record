a868b8bd7a255efdfdea2b9424bebd61bf23a3d5
Kamshory, MT <kamshory@yahoo.com>
2019-09-08 04:49:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,17 +27,19 @@
 <h1>MP3 Recorder</h1>
 	<div class="audio-recorder" data-question-id="12" data-audio-index="1">
     	<div class="audio-tool-wrapper">
-        	<span><a class="audio-tool tool-record"><span></span></a></span>
-        	<span><a class="audio-tool tool-play"><span></span></a></span>
-        	<span><a class="audio-tool tool-stop"><span></span></a></span>
-        	<span><a class="audio-tool tool-upload"><span></span></a></span>
-        	<span><a class="audio-tool tool-download"><span></span></a></span>
-        	<span><a class="audio-tool tool-delete"><span></span></a></span>
+        	<span><a class="audio-tool tool-browse" title="Pilih File Suara"><span></span></a></span>
+        	<span><a class="audio-tool tool-record" title="Rekam Langsung"><span></span></a></span>
+        	<span><a class="audio-tool tool-play" title="Dengarkan"><span></span></a></span>
+        	<span><a class="audio-tool tool-stop" title="Hentikan"><span></span></a></span>
+        	<span><a class="audio-tool tool-upload" title="Unggah Suara"><span></span></a></span>
+        	<span><a class="audio-tool tool-download" title="Unduh Suara"><span></span></a></span>
+        	<span><a class="audio-tool tool-delete" title="Hapus Suara"><span></span></a></span>
             <span><span class="tool-timer"><i></i></span></span>
         </div>
     </div>
 	<div class="audio-recorder" data-question-id="12" data-audio-index="2">
     	<div class="audio-tool-wrapper">
+        	<span><a class="audio-tool tool-browse"><span></span></a></span>
         	<span><a class="audio-tool tool-record"><span></span></a></span>
         	<span><a class="audio-tool tool-play"><span></span></a></span>
         	<span><a class="audio-tool tool-stop"><span></span></a></span>
@@ -53,9 +55,78 @@ var audioRecorder = {
 	initialized:false,
 	recorder:[]
 };
+var params = {
+	bitrate:32,
+	sampleRate:44100,
+	timeLimit:1800,
+	uploadSound:function(audio, duration, index, questionID, audioIndex){
+		console.log(audio.src);
+		audioRecorder.recorder[index].beforeUpload();
+		setTimeout(function(){
+			audioRecorder.recorder[index].afterUpload();
+		}, 2000);
+		var url = audio.src;
+		$.ajax({
+			url:'upload-sound.php',
+			type:'POST',
+			dataType:"json",
+			data:{
+				sound_data:url,
+				question_id:questionID,
+				audio_index:audioIndex
+			},
+			success: function(data)
+			{
+				console.log('Uploaded');
+			}
+		});     
+	},
+	downloadSound:function(audio, duration, index, questionID, audioIndex)
+	{
+		console.log(audio.src);
 
+		//Create a URL to the blob.
+		/*
+		var url = window.URL.createObjectURL(blob);
+		window.open(url);
+		*/
+
+	},
+	nullAudio:function()
+	{
+		alert('Belum ada audio cuyy! Selesaikan rekaman dulu...');
+	},
+	stillRecording:function()
+	{
+		alert('Masih merekam cuyy! Selesaikan dulu yach...');
+	},
+	stillPlaying:function()
+	{
+		alert('Masih memutar cuyy! Ga boleh merekam dulu... Pause dulu kek, atau tunggu selesai...');
+	},
+	overLimit:function()
+	{
+		alert('Melebihi batas');
+	},
+	log:function(text)
+	{
+		console.log(text);
+	}
+};
 
 $(document).ready(function(e) {
+
+	$(document).on('click', '.audio-recorder .tool-browse', function(e)
+	{
+		var audioController = $(this).closest('.audio-recorder');
+		var idx = audioController.attr("data-index") || "";
+		if(idx == "")
+		{
+			var index = audioRecorder.recorder.length;
+			audioRecorder.recorder.push(new MP3Recorder(audioController, index, 'browse', params));
+		}
+		
+	});
 	
 	$(document).on('click', '.audio-recorder .tool-record', function(e)
 	{		
@@ -64,67 +135,7 @@ $(document).ready(function(e) {
 		if(idx == "")
 		{
 			var index = audioRecorder.recorder.length;
-			audioRecorder.recorder.push(new MP3Recorder(audioController, index, {
-				bitrate:32,
-				sampleRate:44100,
-				timeLimit:1800,
-				uploadSound:function(blob, duration, questionID, audioIndex){
-					audioRecorder.recorder[index].beforeUpload();
-					setTimeout(function(){
-						audioRecorder.recorder[index].afterUpload();
-					}, 2000);
-
-
-					var reader = new FileReader();
-					reader.onloadend = function() 
-					{
-						var url = reader.result;
-						$.ajax({
-							url:'upload-sound.php',
-							type:'POST',
-							dataType:"json",
-							data:{
-								sound_data:url,
-								question_id:questionID,
-								audio_index:audioIndex
-							},
-							success: function(data)
-							{
-								console.log('Uploaded');
-							}
-						});     
-					}
-					reader.readAsDataURL(blob); 
-
-				},
-				downloadSound:function(blob, duration, questionID, audioIndex)
-				{
-					//Create a URL to the blob.
-					var url = window.URL.createObjectURL(blob);
-					window.open(url);
-
-				},
-				nullAudio:function()
-				{
-					alert('Belum ada audio cuyy! Selesaikan rekaman dulu...');
-				},
-				stillRecording:function()
-				{
-					alert('Masih merekam cuyy! Selesaikan dulu yach...');
-				},
-				stillPlaying:function()
-				{
-					alert('Masih memutar cuyy! Ga boleh merekam dulu... Pause dulu kek, atau tunggu selesai...');
-				},
-				overLimit:function()
-				{
-					alert('Melebihi batas');
-				},
-				log:function(text)
-				{
-					console.log(text);
-				}
-			}));
+			audioRecorder.recorder.push(new MP3Recorder(audioController, index, 'record', params));
 		}
 	})	
 });
